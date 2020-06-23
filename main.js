7f5d1f90b91a2b56046d95a0e28db14d2a485342
Diego José da Silva <diegojsilvabr@gmail.com>
2020-06-23 18:20:06 -0300

    add dorckerode and upadte assets

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 const { app, Tray } = require('electron');
-const TrayMenu = require('./lib/tray');
+const TrayDocker = require('./lib/tray');
 
 app.on('ready', async () => {
-  new TrayMenu();
-});
-
-process.on('unhandledRejection', function(reason, p){
-  console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
-  // application specific logging here
-});
+    new TrayDocker();
+})
